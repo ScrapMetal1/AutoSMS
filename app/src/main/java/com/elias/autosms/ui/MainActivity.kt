74b6ca04f2ffeffe3dd08859a341f06ccfc75422
ScrapMetal1 <aaronzach11@gmail.com>
2026-02-03 13:00:51 +1100

    Implement Android 12+ Splash Screen API and organize design assets

--- app/src/main/java/com/elias/autosms/ui/MainActivity.kt
+++ app/src/main/java/com/elias/autosms/ui/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.elias.autosms.databinding.ActivityMainBinding
@@ -38,6 +39,7 @@ class MainActivity : AppCompatActivity() {
             }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
